f63edf406c3745744b991a785883d780661225b6
ReyS0926 <rey.sosa26@gmail.comm>
2021-03-15 18:48:37 -0400

    Get Data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,20 @@
-// from data.js
-var tableData = data;
+	// from data.js
+	const tableData = data;
+	
 
-// YOUR CODE HERE!
+	// YOUR CODE HERE!
+	var tbody = d3.select("tbody");
+	
+
+	//Get aata values for each column
+	// 
+
+    data.forEach(row => {
+      var DataRow = tbody.append("tr");
+      Object.entries(row).forEach(([key, value]) => {
+        var cell = DataRow.append("td");
+        cell.text(value);
+      });
+    });
+
+
