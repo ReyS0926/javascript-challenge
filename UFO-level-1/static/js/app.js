7db69aeb1d0f932bda28d03631c0928ef4ac900d
ReyS0926 <rey.sosa26@gmail.comm>
2021-03-15 21:17:48 -0400

    PArt 1 Done

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -17,4 +17,52 @@
       });
     });
 
+	
+    // Select the button
+    var button = d3.select("#filter-btn");
 
+    // Select the form
+    var form = d3.select("#date-form");
+
+    // Create event handlers for clicking the button or pressing the enter key
+    button.on("click", filterDate);
+    form.on("submit", filterDate);
+
+    // Event handler function for the form
+	function filterDate() {
+
+        // Prevent the page from refreshing
+	    d3.event.preventDefault();
+	
+
+	    tbody.html("");
+	
+
+	    // Select the input element and get the raw HTML node
+	    var inputElement = d3.select("#datetime");
+
+	    // Get the value of the input element
+	    var inputValue = inputElement.property("value");
+	    console.log(inputValue);
+	
+
+	    //Filtered data
+	    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+	    console.log(filteredData);
+	
+
+	    filteredData.forEach(function(selections){
+	    
+        //Add one tbale row for each UFO Sighting filtered
+        var row = tbody.append("tr");
+	    
+        //Use Object.entries to append a cell to the row for each value
+        Object.entries(selections).forEach(function([key, value]) {
+	        console.log(key,value);
+	        var cell = row.append("td");
+	        cell.text(value);
+
+            
+	    });
+	    });
+	};
